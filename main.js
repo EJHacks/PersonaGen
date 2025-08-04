c3fe1e53fbf495e48cc5537d44c464ca176a248b
EJHacks <ee1912136@gmail.com>
2025-08-04 17:04:19 -0400

    Co-authored-by: jamalcwoods <jamalcwoods@gmail.com>

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-console.log(data.results)
-console.log(data.results[15])
+let title = document.getElementById("Name");
+let DOB = document.getElementById("DOB");
+let Email = document.getElementById("Email");
+let img = document.getElementById("image");
 
-let randomvalue = Math.random() * data.results.length
-Math.floor(randomvalue);
+// Array Example 
+let var1 = [1,2,3,4,5]
+console.log(var1[0])
+
+
+
+
+let randomvalue = Math.floor(Math.random() * data.results.length)
+let firstPerson = data.results[randomvalue]
+
+title.innerHTML = firstPerson.name.first + " " + firstPerson.name.last;
+DOB.innerHTML = firstPerson.dob.date;
+Email.innerHTML = firstPerson.email;
+img.src = firstPerson.picture.medium;
